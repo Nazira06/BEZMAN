92e4c9abb4a1d9f04c0d709e98321170da046638
nazira <nazikkydyralieva@gmail.com>
2020-12-07 12:48:15 +0600

    user_created, login, logout добавили

--- bezman_shop/accounts/urls.py
+++ bezman_shop/accounts/urls.py
@@ -7,7 +7,9 @@ from .views import *
 urlpatterns = [
     path('customers/', customerList, name='customer'),
     path('customers/<int:customer_id>/', getCustomer, name='getcustomer'),
-    path('user-created/',userCreated)
+    path('user-created/', userCreated),
+    path('login/', auth, name='login'),
+    path('logout/', logout_page, name='logout'),
 
 
 
